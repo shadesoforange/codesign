c593c7abc50ae38bb5cd26e24f10f059c0c42020
Ian Monk <ian@creativedepartment.com>
2014-11-29 15:33:19 -0500

    first commit

---
+++ sites/all/themes/codesign/js/script.js
@@ -0,0 +1,36 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - https://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+
+// To understand behaviors, see https://drupal.org/node/756722#behaviors
+Drupal.behaviors.my_custom_behavior = {
+  attach: function(context, settings) {
+
+    // Place your code here.
+	
+	$('.nav-services ul.menu li a').append('<div class="tag"></div>');
+	$('.nav-services ul.menu li a').hover(
+		function(e){
+			$(this).children('.tag').slideDown(200);
+		},
+		function(e){
+			$(this).children('.tag').slideUp(200);
+		}	
+	);
+	
+  }
+};
+
+
+})(jQuery, Drupal, this, this.document);
