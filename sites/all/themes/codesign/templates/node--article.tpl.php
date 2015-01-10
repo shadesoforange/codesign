7e1ef50031c32156485b2d296092d31186178a9c
Ian Monk <ian@creativedepartment.com>
2015-01-10 12:03:27 -0500

    updates

--- sites/all/themes/codesign/templates/node--article.tpl.php
+++ sites/all/themes/codesign/templates/node--article.tpl.php
@@ -47,3 +47,6 @@
   <?php print render($content['comments']); ?>
 
 </article>
+
+<!-- Go to www.addthis.com/dashboard to customize your tools -->
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-54b15b0710aef927" async="async"></script>
